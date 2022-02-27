4f0231a6b16db4ef5c188007fa72c8af30b88d30
MaxLeBaboul <maxwellbaboula@gmail.com>
2022-02-27 04:27:21 +0100

    connexion à la base de données MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
+
+dotenv.config();
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true },
+    () => {
+      console.log("Connected to MongoDB");
+    }
+  );
+}
 
 app.listen(8800, () => {
   console.log("Backend server is running");
